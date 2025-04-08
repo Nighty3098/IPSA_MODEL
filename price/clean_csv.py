0def89b8b63bd93296c5fc9ce9086cd0f6db14e3
Nighty3098 <night3098games@gmail.com>
2025-04-08 15:38:13 +0700

    Working on new model

--- price/clean_csv.py
+++ price/clean_csv.py
@@ -1,20 +1,15 @@
 import pandas as pd
 
-# Задайте путь к вашему CSV файлу
-input_file = "data.csv"  # замените на имя вашего файла
-output_file = "output.csv"  # имя файла для сохранения очищенных данных
+input_file = "combined_stock_data.csv" 
+output_file = "cleaned_stock_data.csv"
 
-# Чтение CSV файла
 df = pd.read_csv(input_file)
 
-# Оставляем только нужные колонки
 columns_to_keep = ["Ticker", "Date", "Close", "High", "Low", "Open", "Volume"]
 df_cleaned = df[columns_to_keep]
 
-# Удаляем строки с пустыми полями
 df_cleaned = df_cleaned.dropna()
 
-# Сохранение очищенного DataFrame в новый CSV файл
 df_cleaned.to_csv(output_file, index=False)
 
-print(f"Очищенный файл сохранен как {output_file}")
+print(f"Saved as {output_file}")
